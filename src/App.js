4471cd32947cf136e632d7bf1adcaaf9dd86bd9c
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-05-03 22:55:09 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// we will pass an action inside the dispatch, it have 2 properties i-e type and the payload.
+
+// action- it is a plain javaScript object containing the type and the payload.
+
+
+import React from 'react'
+import configureStore from './store/configureStore'
+import { bugAdded } from './store/bugs'
+import { bugResolved } from './store/bugs'
+
+
+const store = configureStore()
+
+
+const unsubscribe = store.subscribe(()=>{
+  console.log('Store changed!', store.getState())
+})
+
+store.dispatch(bugAdded('pehla bug')) 
+store.dispatch(bugAdded('sec bug')) 
+store.dispatch(bugAdded('third bug'))
+store.dispatch(bugAdded('FOURTH bug'))
+
+ 
+
+
+unsubscribe()
+store.dispatch(bugResolved(1))
+
+// store.dispatch(bugResolved())
+
+
+unsubscribe()
+
+// store.dispatch({
+//   type : BUG_REMOVED ,
+//   payload : {
+//     id : 1
+//   }
+// })
+
+
+console.log(store.getState()) 
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/store/bugs.js
@@ -0,0 +1,49 @@
+//types of the actions.
+
+const BUG_ADDED = "bugAdded";
+const BUG_REMOVED = "bugRemoved";
+const BUG_RESOLVED = "bugResolved"
+
+
+// actions creators.
+
+export const bugAdded = (description) =>({
+    type : BUG_ADDED,
+    payload : {
+      description : description
+    } 
+  })
+
+export const bugResolved = (id) =>({
+type : BUG_RESOLVED,
+payload : {
+id : id
+}
+})
+
+
+// Reducer
+
+let lastId = 0
+
+export default function reducer(state=[] , action) {
+    if(action.type === BUG_ADDED){
+        return [
+            ...state, 
+            {
+                id: ++lastId, 
+                description : action.payload.description,
+                resolved : false
+            }
+        ]
+    }
+    else if (action.type === BUG_REMOVED){
+        return state.filter((bug)=> bug.id!== action.payload.id)
+    }
+    else if(action.type === BUG_RESOLVED){
+       return state.map(bug=> bug.id !== action.payload.id? bug : {...bug , resolved : true})
+    }
+    else {
+        return state
+    }
+}
